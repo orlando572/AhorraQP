35d587b8ffeb1c0da08a67e744e216862974e77d
orlando572 <orlandooficial021@gmail.com>
2025-11-28 23:45:20 -0500

    Se creo script para subir precios a la db

--- backend/app/scrapers/__init__.py
+++ backend/app/scrapers/__init__.py
@@ -5,4 +5,4 @@ Web scrapers para diferentes supermercados
 from app.scrapers.tottus_scraper import TottusScraper
 from app.scrapers.plaza_vea_scraper import PlazaVeaScraper
 
-__all__ = ["TottusScraper", "PlazaVeaScraper"]
+__all__ = ["TottusScraper", "PlazaVeaScraper", "MakroScraper"]
